8f5b3b8158f21ff0caa56836d28643d8b6a1b87f
diljara <gizatullina.dr@phystech.edu>
2021-11-08 17:26:15 +0300

    data to file

--- solar_input.py
+++ solar_input.py
@@ -90,8 +90,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME!
+            out_file.write(obj.type, obj.R, obj.m, obj.x, obj.y, obj.Vx, obj.Vy)
+            
 
 
 if __name__ == "__main__":
