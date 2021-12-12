6ff10a32f25d687a4305aaa88613610bd20c49d5
petrk <kovalev.pa@phystech.edu>
2021-12-13 02:51:36 +0300

    bruh

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ def main():
     while alive:
         handle_events(pg.event.get(), menu)
         cur_time = time.perf_counter()
-        write_space_objects_data_to_file("stats.txt", space_objects)
+
         if perform_execution:
             execution((cur_time - last_time) * time_scale)
             text = "%d seconds passed" % (int(model_time))
--- visual.py
+++ visual.py
@@ -2,7 +2,10 @@
 # license: GPLv3
 
 import pygame as pg
-
+import objects
+import os
+from math import sin, cos, tan as sin, cos, tan
+from numpy import pi as pi
 """Модуль визуализации.
 Нигде, кроме этого модуля, не используются экранные координаты объектов.
 Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
@@ -23,6 +26,8 @@ scale_factor = 1
 Тип: float
 
 Мера: количество пикселей на один метр."""
+
+
 
 
 def calculate_scale_factor(max_distance):
@@ -74,18 +79,72 @@ class Drawer:
         рисует все объекты из заданного массива'''
         self.screen.fill((0, 0, 0))
         for figure in figures:
-            figure.draw(self.screen)
-
+            if figure.obj.type == "Starship":
+                figure.draw_starship(self.screen)
+            else:
+                figure.draw(self.screen)
+            if figure.obj.type != 'CelestialBody' and figure.obj.type != 'Lazer_beam':
+                figure.draw_hp(self.screen)
         screen.blit()
         screen.update()
         pg.display.update()
 
 
-class DrawableObject:
+class DrawableObject(pg.sprite.Sprite):
     def __init__(self, obj):
         self.obj = obj
+        #pg.sprite.Sprite.__init__(self)
+        #self.image = starship_img = pg.image.load(os.path.join(r'C:\Users\petrk\among_stars\img', 'rock.png')).convert()
+        #self.image.set_colorkey((0, 0, 0))
+        #self.rect = self.image.get_rect()
+        #self.rect.center = (300, 300)
 
     def draw(self, surface):
         '''рисует круглый объект на заданной поверхности, 
         используя параметры объекта: радиус, цвет, местоположение'''
         pg.draw.circle(surface, self.obj.color, (scale_x(self.obj.x), scale_y(self.obj.y)), self.obj.R)
+
+    def draw_hp(self, surface):
+        R = self.obj.R
+        x = scale_x(self.obj.x)
+        y = scale_y(self.obj.y)
+        pg.draw.rect(surface, (0, 255, 0),[x - 20, y - R - 5, 40 * (self.obj.HP/ (objects.HPCONST * self.obj.m)), 3])
+
+    def draw_starship(self, surface):
+
+        w = 6
+        pi = 3.14159
+
+        an0 = 2 * pi / 9
+
+        r = self.obj.R
+        r2 = self.obj.R / sin((pi - an0) / 2)
+        an1 = 3 * pi / 4 - an0/4
+
+        angle = self.obj.angle
+
+        an2 = an1 + self.obj.angle
+        an3 = an1 - self.obj.angle
+
+        x = scale_x(self.obj.x)
+        y = scale_y(self.obj.y)
+
+        #hitbox
+        pg.draw.circle(surface, self.obj.color, (x, y), self.obj.R)
+
+
+        #visual body
+        #pg.draw.polygon(surface, self.obj.color, [(x + 2 * r * cos(angle), y + 2 * r * sin(angle)),
+         #            (x - r * cos(angle) + w * cos(angle + pi/2), y - r * sin(angle) + w * sin(angle + pi/2)),
+          #           (x - r * cos(angle) + w * cos(angle - pi/2), y - r * sin(angle) + w * sin(angle - pi/2))])
+        #pg.draw.polygon(surface, self.obj.color, [(x + 10, y ),
+        #                     (x - 10.232323, y + 10),
+        #                     (x - 10, y - 10)])
+#
+        #thruster
+        k = 0.1
+        tr_h = 10
+        tr_w = 3
+        pg.draw.polygon(surface, (255,0,0), [(x, y),
+                                             (x + tr_h * cos(self.obj.angle + 3*pi/4), y + tr_h * sin(self.obj.angle + 3*pi/4) ),
+                                             (x + tr_h * cos(self.obj.angle - 3*pi/4), y + tr_h * sin(self.obj.angle - 3*pi/4) )])
